25e89164da29f423694353061cd39a7f4e0c965d
Mateusz Curylo <mateusz.curylo@gl05654m.gamesys.corp>
2017-10-12 09:49:48 +0100

    Final sol

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,11 @@ import {fmap} from './category';
 import {Func1} from './utils';
 import {string2RegExp} from 'fuse-box/dist/typings/Utils';
 
-
-
 const head = <T>(arr: T[]): Maybe<T> => Just.of(arr[0]);
 const greet = (name: string) => `Hello! ${name}`;
 const failMessage = 'What is your name stranger?';
 const drop = <T> (num: number) => (arr: T[]) => arr.slice(num);
 
-console.log(process.argv);
 readArgs
     .map(drop(2))
     .map(head)
